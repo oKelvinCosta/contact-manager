4662dc9cf28bae9e84c0430eea29a0496025950f
Kelvin Costa <okelvincosta@gmail.com>
2019-06-11 13:48:51 -0300

    Build

---
+++ precache-manifest.82306a56b74ff3523de4ff71d89c0065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "336ea387df67a611f30cce15e045ddc7",
+    "url": "/contact-manager/index.html"
+  },
+  {
+    "revision": "2e569491c0c8b3f585c5",
+    "url": "/contact-manager/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "5c847cba8117e64c6ca7",
+    "url": "/contact-manager/static/css/main.6fbab4eb.chunk.css"
+  },
+  {
+    "revision": "2e569491c0c8b3f585c5",
+    "url": "/contact-manager/static/js/2.9efe076d.chunk.js"
+  },
+  {
+    "revision": "5c847cba8117e64c6ca7",
+    "url": "/contact-manager/static/js/main.fd46c243.chunk.js"
+  },
+  {
+    "revision": "a25dc548b2853493d42e",
+    "url": "/contact-manager/static/js/runtime~main.b61356a3.js"
+  }
+]);
--- src/components/Colors.js
+++
@@ -1,7 +0,0 @@
-const Colors = {
-  primary: "#2196f3",
-  secondary: "#ff1744",
-  terciary: "#1769aa"
-};
-
-export default Colors;
